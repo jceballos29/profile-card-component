abf2374c8440050db459241e5bb48c9697eaaf18
Juan Ceballos <jacu29@gmail.com>
2022-02-18 01:27:26 -0500

    Profile Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import header from "./images/bg-pattern-card.svg";
+import victor from "./images/image-victor.jpg";
+
+import top from "./images/bg-pattern-top.svg";
+import bottom from "./images/bg-pattern-bottom.svg"
+
+const footerContent = [
+    { number: "80K", title: "Followers" },
+    { number: "803K", title: "Likes" },
+    { number: "1.4K", title: "Photos" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img
+                src={top}
+                alt="Decoration Top"
+                className="app__decoration__top"
+            />
+            <img
+                src={bottom}
+                alt="Decoration Bottom"
+                className="app__decoration__bottom"
+            />
+
+            <div className="card">
+                <img src={header} alt="Profile Card" className="card__header" />
+                <div className="card__body">
+                    <img src={victor} alt="Victor Crest" />
+                    <h1>
+                        Victor Crest <span>26</span>
+                    </h1>
+                    <p>London</p>
+                </div>
+                <div className="card__footer">
+                    {footerContent.map((item) => (
+                        <div className="card__footer__item">
+                            <h2>{item.number}</h2>
+                            <span>{item.title}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
